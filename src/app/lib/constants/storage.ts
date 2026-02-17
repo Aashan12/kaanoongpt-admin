fd0b77ce3b2e5ed2408ea698bb35118ed61f6ca0
Aashan12 <Nagarkotiaashan1@gmail.com>
2026-02-17 23:07:46 +0545

    feat: add local storage keys

---
+++ src/app/lib/constants/storage.ts
@@ -0,0 +1,9 @@
+export const STORAGE_KEYS = {
+  ADMIN_TOKEN: 'adminToken',
+  ADMIN_USER: 'adminUser',
+  THEME: 'theme',
+  LANGUAGE: 'language',
+  SIDEBAR_COLLAPSED: 'sidebarCollapsed',
+  RECENT_SEARCHES: 'recentSearches',
+  USER_PREFERENCES: 'userPreferences',
+} as const;
